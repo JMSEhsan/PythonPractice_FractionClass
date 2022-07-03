8edd7eb1eebb847102f00182024af9748049879a
Ehsan Jalali <JMSEhsan@gmail.com>
2022-07-03 01:04:29 -0600

    commit

--- FractionClass.py
+++ FractionClass.py
@@ -1,8 +1,15 @@
-
-from curses.panel import bottom_panel, top_panel
-
+# Fraction Class
 
 class Fraction(object):
-    def__init__(self, top, bottom):
-    self.top = top
-    self.bottom = bottom
+    def __init__(self, top, bottom):
+      self.top = top
+      self.bottom = bottom
+    
+   # def __add__(self, other_fraction):
+    def __str__(self):
+      return str(self.top)+"/"+str(self.bottom)
+half = Fraction (1, 2)
+quarter = Fraction (1, 4)
+print(half)
+print(quarter)
+
